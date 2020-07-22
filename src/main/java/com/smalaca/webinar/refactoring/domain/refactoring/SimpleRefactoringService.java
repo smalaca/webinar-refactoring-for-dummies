d9603ab13cf4ef4fe6e6676239075490d3487e77
smalaca <ab13.krakow@gmail.com>
2020-07-22 18:58:42 +0200

    replace two class attributes with one

--- src/main/java/com/smalaca/webinar/refactoring/domain/refactoring/SimpleRefactoringService.java
+++ src/main/java/com/smalaca/webinar/refactoring/domain/refactoring/SimpleRefactoringService.java
@@ -6,18 +6,13 @@ import com.smalaca.webinar.refactoring.domain.developer.Developer;
 import com.smalaca.webinar.refactoring.domain.testssuite.TestsSuiteRepository;
 
 public class SimpleRefactoringService {
-    private final TestsSuiteRepository testsSuiteRepository;
-    private final CodeBaseRepository codeBaseRepository;
+    private final IsRequiredToStartRefactoring isRequiredToStartRefactoring;
 
     SimpleRefactoringService(TestsSuiteRepository testsSuiteRepository, CodeBaseRepository codeBaseRepository) {
-        this.testsSuiteRepository = testsSuiteRepository;
-        this.codeBaseRepository = codeBaseRepository;
+        isRequiredToStartRefactoring = new IsRequiredToStartRefactoring(codeBaseRepository, testsSuiteRepository);
     }
 
     public void apply(ClassCode code, Developer developer) {
-        IsRequiredToStartRefactoring isRequiredToStartRefactoring = new IsRequiredToStartRefactoring(
-                codeBaseRepository, testsSuiteRepository);
-
         if (isRequiredToStartRefactoring.check(code)) {
             startRefactoringOf(code, developer);
         }
